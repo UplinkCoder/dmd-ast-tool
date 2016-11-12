3376f93d374aa030081ef12809579093b304ecd5
Stefan Koch <uplink.coder@googlemail.com>
2016-11-12 06:08:04 +0000

    reformat

--- app.d
+++ app.d
@@ -6,7 +6,7 @@ import std.file;
 import std.math;
 
 string visitor_boilerplate = `
-odule ddmd.asttypename;
+module ddmd.asttypename;
 
 import ddmd.attrib;
 import ddmd.aliasthis;
@@ -52,7 +52,7 @@ string astTypeName(RootObject node)
             return astTypeName(cast(Type)node);
         case DYNCAST_EXPRESSION:
             return astTypeName(cast(Expression)node);
-	    default : assert(0, "don't know this DYNCAST");
+        default : assert(0, "don't know this DYNCAST");
     }
 }
 
@@ -107,24 +107,24 @@ extern(C++) final class AstTypeNameVisitor : Visitor
 {
     alias visit = super.visit;
 public :
-        string typeName;
+    string typeName;
 `;
 
-struct Entry 
+struct Entry
 {
     uint level;
     string nodeName;
+    uint idx;
     // populated after populate was called
     uint parentIdx;
     uint numberOfChildren;
 
-    uint importantParentIdx;
     uint enumIdx;
 }
 
 Entry[] entries;
 
-string[5] importantParents = ["Dsymbol","Type","Expression","Statement","TemplateDeclaration"];
+string[5] importantParents = ["Dsymbol", "Type", "Expression", "Statement", "TemplateDeclaration"];
 uint[5] importantParentIdxs;
 /*ClassDeclaration[] extractClassDeclarations(string code)
 {
@@ -138,103 +138,114 @@ uint[5] importantParentIdxs;
 
 }*/
 
-void main() {
+void main()
+{
     string ch_txt = readText("ch.txt");
-    auto lines = ch_txt.splitLines; 
+    auto lines = ch_txt.splitLines;
 
-    foreach(i,line;lines)
+    foreach (i, line; lines)
     {
         auto level = countTabs(line);
         line = line[level .. $];
-        if (!line.length) continue;
+        if (!line.length)
+            continue;
         auto nameString = line.split(' ')[0];
 
-	if(nameString.length)
-            entries ~= Entry(level, nameString);
+        if (nameString.length)
+            entries ~= Entry(level, nameString, cast(uint) i);
     }
 
     writeln("The DMD class hierachy has ", entries.length, " members");
 
-    foreach(i; 0 .. entries.length)
-        populate(cast(uint)i);
-
+    foreach (i; 0 .. entries.length)
+        populate(cast(uint) i);
 
     uint typeCount[6];
-    foreach(i, entry;entries)
+    foreach (i, entry; entries)
     {
-     //   if (canFind()) writeln(entry);
-        typeCount[entry.level-1]++;
+        //   if (canFind()) writeln(entry);
+        typeCount[entry.level - 1]++;
     }
-   writeln(genTypeStringVisitor());
+    //writeln(genTypeStringVisitor());
 
-//   writeln(typeCount/*[].map!(i => cast(int)(log2(i)+0.5))*/);
+    //   writeln(typeCount/*[].map!(i => cast(int)(log2(i)+0.5))*/);
     uint pidx;
-    foreach(i, entry;entries)
+    foreach (i, entry; entries)
     {
-     if (entry.nodeName == importantParents[pidx])   
-        importantParentIdxs[pidx++] = cast(uint)i;       
+        if (entry.nodeName == importantParents[pidx])
+            importantParentIdxs[pidx++] = cast(uint) i + 1;
     }
     pidx = 0;
-    
-    foreach(i, entry;entries)
-    {   // let's now compute the enumIdx
+
+    foreach (i, entry; entries)
+    { // let's now compute the enumIdx
         if (importantParentIdxs[].canFind(i))
-        { 
+        {
             entry.enumIdx = 1 << pidx++;
             continue;
         }
         else
         {
-            Entry ipe = entry;  
-            while(ipe.level > 2)
+            Entry ipe = entry;
+            while (ipe.level > 2)
             {
-                ipe = entries[ipe.parentIdx];
+                ipe = entries[ipe.parentIdx - 1];
             }
-            
+
         }
-        
+
     }
 }
 
 string genTypeStringVisitor()
 {
-   string result;
+    string result;
 
-   result ~= "\n" ~ (visitor_boilerplate);
-   foreach(entry;entries)
-   {
-      {
-          result ~= "\n";
-          result ~= "\n" ~ ("    override void visit(" ~ entry.nodeName ~ " node)");
-          result ~= "\n" ~ ("    {");
-          result ~= "\n" ~ ("        typeName = \"" ~ entry.nodeName ~ "\";");
-          result ~= "\n" ~ ("    }");
-      }
-   }   
+    result ~= "\n" ~ (visitor_boilerplate);
+    foreach (entry; entries)
+    {
+        {
+            result ~= "\n";
+            result ~= "\n" ~ ("    override void visit(" ~ entry.nodeName ~ " node)");
+            result ~= "\n" ~ ("    {");
+            result ~= "\n" ~ ("        typeName = \"" ~ entry.nodeName ~ "\";");
+            result ~= "\n" ~ ("    }");
+        }
+    }
 
-   return result;
+    return result;
 }
 
 void populate(uint entryIdx)
 {
-        auto entryLevel = entries[entryIdx].level;
-        if(entryLevel > 1) foreach_reverse(i, entry; entries[0 .. entryIdx])
+    auto entryLevel = entries[entryIdx].level;
+    if (entryLevel > 1)
+        foreach_reverse (i, entry; entries[0 .. entryIdx])
         {
-            if(entryLevel > entry.level)
+            if (entryLevel > entry.level)
             {
-                entries[entryIdx].parentIdx = cast(uint)i;
+                entries[entryIdx].parentIdx = cast(uint) i + 1;
                 ++entries[i].numberOfChildren;
-		return ;
+                return;
             }
         }
 
-	return ;
+    return;
 }
 
 uint countTabs(ref string line)
 {
     int numTabs;
-    while(line.length && line[numTabs] == '\t')  {++numTabs;}
+    while (line.length && line[numTabs] == '\t')
+    {
+        ++numTabs;
+    }
     return numTabs;
 }
 
+void GatherDefinitionLocations()
+{
+    // we need to match the regex "extern (C++)" * "class" \s some_name_in_list *
+
+    //	extern(C++) 
+}
